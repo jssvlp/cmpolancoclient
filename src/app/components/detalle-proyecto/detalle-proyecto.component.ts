2679fac8d932a5d4273552571898aa5563ed6a75
pjms28 <pjms_28@hotmail.com>
2019-05-10 19:15:20 -0400

    Correcciones de moneda, ortografia en detalle proyecto

--- src/app/components/detalle-proyecto/detalle-proyecto.component.ts
+++ src/app/components/detalle-proyecto/detalle-proyecto.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ProyectoService } from 'src/app/services/proyecto.service';
 import { ActivatedRoute } from '@angular/router';
 import { ProyectoModel } from 'src/app/model/Proyecto.model';
+import { CaracteristicaModel } from 'src/app/model/Caracteristica.model';
+import { CaracteristicaService } from 'src/app/services/caracteristica.service';
 declare var $:any;
 @Component({
   selector: 'app-detalle-proyecto',
@@ -12,16 +14,31 @@ export class DetalleProyectoComponent implements OnInit {
 
   ID: any;
   data: ProyectoModel;
-  
-  constructor(private proApi: ProyectoService, private actvRoute: ActivatedRoute ) { }
+  seguridad: CaracteristicaModel[];
+  distribucion: CaracteristicaModel[];
+  amenidades: CaracteristicaModel[];
+  descripcionG: CaracteristicaModel[];
+  descripcionA: CaracteristicaModel[];
+  otros:CaracteristicaModel[];
+
+  constructor(private proApi: ProyectoService, private actvRoute: ActivatedRoute, private carApi: CaracteristicaService ) { }
 
   ngOnInit() {
     this.ID = this.actvRoute.snapshot.paramMap.get(' id');
 
     this.proApi.getProject(this.ID).subscribe(res =>{
-      this.data = res;
-      console.log(this.data);
+      this.data = res;      
     })
+
+    this.carApi.getCarProyecto(this.ID).subscribe(res =>{
+      this.amenidades = res.amenidades;
+      this.descripcionA = res.descripcionA;
+      this.descripcionG = res.descripcionG;
+      this.distribucion = res.distribucion;
+      this.otros = res.otros;
+      this.seguridad = res.seguridad;
+    })
+
 
     $(document).ready(function(){
       $(window).scroll(function(){
