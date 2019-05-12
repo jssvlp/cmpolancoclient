2c9d7f6a48e76b51f6883f400740b895a1a9cdc4
Kevin Reyes <36552064+kfreyes@users.noreply.github.com>
2019-05-12 14:18:15 -0400

    Cambios en la vista Nosotros

--- src/app/components/nosotros/nosotros.component.ts
+++ src/app/components/nosotros/nosotros.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+declare var $:any;
 
 @Component({
   selector: 'app-nosotros',
@@ -10,6 +11,20 @@ export class NosotrosComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    $(document).ready(function(){
+      $(window).scroll(function(){
+        var scroll = $(window).scrollTop();
+        if (scroll >10) {
+          $("#navbarSupportedContent").css("background" , "linear-gradient(to bottom, #2c2a22  0%, #2c2a22 100%)");
+        }
+        else{
+          $("#navbarSupportedContent").css("background" , "transparent");
+
+        }
+      })
+    })
+
+    
   }
 
 }
