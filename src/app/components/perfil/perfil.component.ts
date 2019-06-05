aa0b59d60b366d5d4e6d22b41c4f3e43a26744c4
Kevin Reyes <36552064+kfreyes@users.noreply.github.com>
2019-06-05 17:02:20 -0400

    Cambio del color del navbar del perfil

--- src/app/components/perfil/perfil.component.ts
+++ src/app/components/perfil/perfil.component.ts
@@ -3,6 +3,8 @@ import { AuthService } from 'src/app/services/auth.service';
 import { database } from 'firebase';
 import { UserModel } from 'src/app/model/User.model';
 import { SolicitudModel } from 'src/app/model/Solicitud.model';
+declare var $:any;
+
 
 @Component({
   selector: 'app-perfil',
@@ -23,5 +25,17 @@ export class PerfilComponent implements OnInit {
       this.solicitud = this.data.solicitud;
     });
     
+    $(document).ready(function(){
+      $(window).scroll(function(){
+        var scroll = $(window).scrollTop();
+        if (scroll >0) {
+          $("#navbarSupportedContent").css("background" , "linear-gradient(to bottom, #2c2a22  0%, #2c2a22 100%)");
+        }
+        else{
+          $("#navbarSupportedContent").css("background" , "transparent");
+
+        }
+      })
+    })
   }
 }
